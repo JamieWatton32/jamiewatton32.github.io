27df14ebbf6c03cccabaf5710caec874a2aad215
JamieWatton32 <jamiewatton199559955@gmail.com>
2024-03-07 22:48:54 -0400

    fixed skills container

--- scripts/toggleDiv.js
+++ scripts/toggleDiv.js
@@ -1,6 +1,6 @@
 function toggleDiv(divId) {
   document.getElementById("about-container").style.display = "none";
-  document.getElementById('').style.display = "none";
+  document.getElementById('skills-container').style.display = "none";
   document.getElementById("experience-container").style.display = "none";
   document.getElementById("contact-container").style.display = "none";
   document.getElementById("projects-container").style.display = "none";
